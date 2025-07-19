a1dcd17612a3cfdcfc24c5f3fc864e1a0727c0af
AmandaNbr <amandapnobre@gmail.com>
2025-07-18 23:14:21 -0300

    Update yamels file to use elastic search, kafka and kibana

--- kubernetes/init_cluster.sh
+++ kubernetes/init_cluster.sh
@@ -1,19 +1,15 @@
 #!/bin/bash
 
-echo "[1/4] Inicializando cluster com kubeadm..."
+echo "[1/3] Inicializando cluster com kubeadm..."
 kubeadm init --pod-network-cidr=10.244.0.0/16 --node-name=$(hostname)
 
-echo "[2/4] Configurando kubectl para o root..."
+echo "[2/3] Configurando kubectl para o root..."
 mkdir -p $HOME/.kube
 cp -f /etc/kubernetes/admin.conf $HOME/.kube/config
 chown $(id -u):$(id -g) $HOME/.kube/config
 
-echo "[3/4] Instalando Flannel CNI..."
+echo "[3/3] Instalando Flannel CNI..."
 kubectl apply -f https://raw.githubusercontent.com/flannel-io/flannel/master/Documentation/kube-flannel.yml
-
-echo "[4/4] Instalando pods..."
-kubectl apply -f stack.yaml
-
 
 echo ""
 echo "✅ Cluster Kubernetes inicializado com sucesso!"
