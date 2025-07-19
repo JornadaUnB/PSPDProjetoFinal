765e385c57f3ffdcf061181b9628342acb0e6172
Leonardo Takehana <Leonardo Takehana>
2025-07-18 23:35:50 -0300

    topico

--- spark-engine/src/main/scala/GameOfLifeSpark.scala
+++ spark-engine/src/main/scala/GameOfLifeSpark.scala
@@ -56,7 +56,7 @@ object GameOfLifeSpark {
   def createKafkaConsumer(): KafkaConsumer[String, String] = {
     val props = new Properties()
     props.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, "kafka:9092")
-    props.put(ConsumerConfig.GROUP_ID_CONFIG, "game-of-life-group")
+    props.put(ConsumerConfig.GROUP_ID_CONFIG, "spark-topic")
     props.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, "org.apache.kafka.common.serialization.StringDeserializer")
     props.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, "org.apache.kafka.common.serialization.StringDeserializer")
     props.put(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "earliest")
